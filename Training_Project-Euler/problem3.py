1f7d6019991496dd4387e45e5b03114484e3bc7b
machidd <160207263+machidd@users.noreply.github.com>
2024-02-25 23:40:38 +0900

    Update problem3.py

--- Training_Project-Euler/problem3.py
+++ Training_Project-Euler/problem3.py
@@ -1,3 +1,19 @@
 #13195 の素因数は 5, 7, 13, 29 である.
 
 #600851475143 の素因数のうち最大のものを求めよ.
+n = 600851475143
+a = []
+while n % 2 == 0:
+    a.append(2)
+    n //= 2
+f = 3
+while f * f <= n:
+    if n % f == 0:
+        a.append(f)
+        n //= f
+    else:
+        f += 2
+if n != 1:
+    a.append(n)
+a.sort(reverse=True)
+print(a[0])
