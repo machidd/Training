c0edac9ab44c63e2dde5df5f5cb4d69d7328b9c5
machidd <160207263+machidd@users.noreply.github.com>
2024-02-26 08:58:54 +0900

    Update problem4.py

--- Training_Project-Euler/problem4.py
+++ Training_Project-Euler/problem4.py
@@ -1,2 +1,16 @@
 #左右どちらから読んでも同じ値になる数を回文数という. 2桁の数の積で表される回文数のうち, 最大のものは 9009 = 91 × 99 である.
 #では, 3桁の数の積で表される回文数の最大値を求めよ.
+
+a = 999
+b = 999
+
+while a > 0:
+    ans = str(a * b)
+    length = len(ans)
+    if length < 6:
+        break
+    if int(ans[0]) == int(ans[5]):
+        print(ans)
+        print(length)
+    a -= 1
+    b -= 1
