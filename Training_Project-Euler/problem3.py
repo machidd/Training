ef3d8f9817e2ed7711f3f49638f1c2e4eb4ef51b
machidd <persephone66634@gmail.com>
2024-02-25 22:36:23 +0900

    problem3

--- Training_Project-Euler/problem3.py
+++ Training_Project-Euler/problem3.py
@@ -1,3 +1,24 @@
 #13195 の素因数は 5, 7, 13, 29 である.
 
 #600851475143 の素因数のうち最大のものを求めよ.
+
+#https://odz.sakura.ne.jp/projecteuler/
+
+n = 600851475143
+
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+    print(a)
