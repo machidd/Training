d4fd1a0765c5b4f448c6644e3b3ce7c36f335d84
Keisuke <k.iwamura@ultprice.com>
2024-02-29 18:00:45 +0900

    feat: add regular expression training problems

---
+++ training-regular-expression/problem4.py
@@ -0,0 +1,6 @@
+"""
+問題4: HTMLタグの除去
+与えられたHTML文字列から全てのHTMLタグを除去してください。
+"""
+
+given_text = "<html><head><title>タイトル</title></head><body>この部分が本文です。</body></html>"
---
+++ training-regular-expression/problem8.py
@@ -0,0 +1,6 @@
+"""
+問題8: URLの検出
+与えられた文字列からURLを全て検出してください。URLは、httpまたはhttpsで始まるものとします。
+"""
+
+given_text = "参考になるウェブサイトはhttps://www.example.comとhttp://www.example.orgです。"
