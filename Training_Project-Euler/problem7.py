0f88c234b3ecbf608df894284c514264af15a583
machidd <persephone66634@gmail.com>
2024-02-29 11:02:06 +0900

    問題追加

--- Training_Project-Euler/problem7.py
+++ Training_Project-Euler/problem7.py
@@ -4,3 +4,13 @@
 10 001 番目の素数を求めよ.
 """
 
+ans = []
+for i in range(2, 10000):
+    flag = True
+    for j in range(2, i):
+        if i % j ==0:
+            flag = False
+    if flag == True:
+        ans.append(i)
+print(ans)
+#print(ans[1000])
