1efd6ef52df852085e5e22d8ffdf4650d744aaae
machidd <persephone66634@gmail.com>
2024-03-07 20:11:49 +0900

    P16回答

--- Training_Project-Euler/problem16.py
+++ Training_Project-Euler/problem16.py
@@ -3,3 +3,16 @@
 
 同様にして, 2の1000乗 の各位の数字の和を求めよ.
 """
+j = 0
+ans = 0
+
+for i in range(1,1001):
+    j = 2 ** i
+
+newj = str(j)
+
+for i in range(len(newj)):
+    ans += int(newj[i])
+
+print(ans)
+
