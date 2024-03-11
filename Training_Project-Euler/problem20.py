5497d46927e1ebf76649795526c6233d5dbc3fab
machidd <persephone66634@gmail.com>
2024-03-11 23:46:33 +0900

    P20回答

--- Training_Project-Euler/problem20.py
+++ Training_Project-Euler/problem20.py
@@ -5,6 +5,18 @@ n × (n - 1) × ... × 3 × 2 × 1 を n! と表す.
 この数の各桁の合計は 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27 である.
 
 では, 100! の各位の数字の和を求めよ.
-
-注: Problem 16 も各位の数字の和に関する問題です。解いていない方は解いてみてください。
 """
+
+num = 100
+val = 1
+ans = 0
+
+for i in range(num, 1, -1):
+    val *= i
+
+newval = str(val)
+
+for i in range(len(newval)):
+   ans += int(newval[i]) 
+
+print(ans)
