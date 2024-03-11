c8f8b690ef966b07ce80f517f980c76ea65e9544
machidd <persephone66634@gmail.com>
2024-03-11 23:49:03 +0900

    P2回答

--- training-regular-expression/problem2.py
+++ training-regular-expression/problem2.py
@@ -1,4 +1,5 @@
 """
+解けた
 問題2: メールアドレスの検出
 与えられた文字列からメールアドレスを全て検出してください。メールアドレスは、一般的な形式（例: username@example.com）に従っているとします。
 """
@@ -6,5 +7,5 @@ import re
 
 given_text = "連絡先はtaro@example.com、またはhanako@example.co.jpです。"
 
-ans = re.findall(r"[a-z]+@example.[a-z.]+" , given_text)
+ans = re.findall(r"[a-z1-9]+@[a-z]+.[a-z.]+" , given_text)
 print(ans)
