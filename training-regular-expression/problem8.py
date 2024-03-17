d5708adb4af10db97ee81eba599543be54252fb2
machidd <persephone66634@gmail.com>
2024-03-17 21:31:47 +0900

    P8回答

--- training-regular-expression/problem8.py
+++ training-regular-expression/problem8.py
@@ -1,10 +1,10 @@
 """
-雑な指定をしたい時の正規表現方法がわからない
+解けた
 問題8: URLの検出
 与えられた文字列からURLを全て検出してください。URLは、httpまたはhttpsで始まるものとします。
 """
 import re
 
 given_text = "参考になるウェブサイトはhttps://www.example.comとhttp://www.example.orgです。"
-word = r"https*:\/\/[\w+.\w+.+]"
+word = r"https*:\/\/[a-z.]+"
 print(re.findall(word ,given_text))
