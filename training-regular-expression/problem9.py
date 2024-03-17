26973da3f85946e3e271b1abd679d6e31e0cbee8
machidd <persephone66634@gmail.com>
2024-03-17 21:33:38 +0900

    P9回答

--- training-regular-expression/problem9.py
+++ training-regular-expression/problem9.py
@@ -1,4 +1,5 @@
 """
+解けた
 問題9: 数字のみを含む行の検出
 与えられた複数行の文字列から、数字のみを含む行を全て検出してください。
 """
@@ -6,4 +7,5 @@ import re
 
 given_text = "これはサンプルテキストです。\n12345\nテキストと数字123\n67890\n最後の行です。"
 
-word = r""
+word = r"^[0-9]+"
+print(re.findall(word, given_text, re.MULTILINE))
